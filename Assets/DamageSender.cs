faa0d9de1f93e2f96ad58fb9cec0b17a838f1f2d
trung.hoang <trung.hoang@sotatek.com>
2022-11-08 15:44:34 +0700

    e9: create enemy controller

--- Assets/DamageSender.cs
+++ Assets/DamageSender.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DamageSender : MonoBehaviour
 {
+    protected EnemyCtrl enemyCtrl;
+
+    private void Awake()
+    {
+        this.enemyCtrl = GetComponent<EnemyCtrl>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageReceiver damageReceiver = collision.GetComponent<DamageReceiver>();
         damageReceiver.Receive(1);
+        this.enemyCtrl.despawner.Despawn();
     }
 
 }
